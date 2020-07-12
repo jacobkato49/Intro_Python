214e3c8fd3fa170de8ac6f35538f48012872ef75
Jacob Kato <jacobkato49@yahoo.com>
2020-07-12 12:57:30 -0700

    created a while loop for guesses

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 #import random module
 import random
 
-# Getting the users guess
-guess = int(input("Enter your guess between 1 and 100: ")) #make sure user enters integer
+
 
 #generate a random number between 1 and 100
 randnum = random.randint(1,101)
 
-if guess < randnum:
-    print('higher')
-elif guess > randnum:
-    print('lower')
-else:
-    print('Congratulations, you guessed the number')
+count = 0
+guess = -99
+
+while (guess != randnum):
+
+
+    # Getting the users guess
+    guess = int(input("Enter your guess between 1 and 100: ")) #make sure user enters integer
+    if guess < randnum:
+        print('higher')
+    elif guess > randnum:
+        print('lower')
+    else:
+        print('Congratulations, you guessed the number')
+        break
+    count = count + 1
+# end the while loop
+
+print('You took ' + str(count) + ' tries to guess the number.')
