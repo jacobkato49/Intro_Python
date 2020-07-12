ea19e2ddbca6565421fe54196b8586100a6c6208
Jacob Kato <jacobkato49@yahoo.com>
2020-07-12 12:48:49 -0700

    added conditional statements for the user to guess the number

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
+#import random module
+import random
+
 # Getting the users guess
-guess = input("Enter your guess between 1 and 100")
+guess = int(input("Enter your guess between 1 and 100: ")) #make sure user enters integer
+
+#generate a random number between 1 and 100
+randnum = random.randint(1,101)
+
+if guess < randnum:
+    print('higher')
+elif guess > randnum:
+    print('lower')
+else:
+    print('Congratulations, you guessed the number')
